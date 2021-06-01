361ca1be0849aac6ed3ea76015c9c2ee44799070
JJCone <jackson.j.cone@gmail.com>
2021-06-01 12:29:20 -0500

    Update whichData.m
    edited directory for JJCs machine

---  Analysis/Analysis Code/whichData.m
+++  Analysis/Analysis Code/whichData.m
@@ -1,4 +1,7 @@
 function [dirName, tableName] = whichData()
 
-tableName = sprintf(' Analysis/Mat Files/masterTable.mat');
-dirName = '/Users/Shared/Data/OKernel/';
+% tableName = sprintf('Analysis/Mat Files/masterTable.mat');
+% dirName = '/Users/Shared/Data/SCernel/';
+
+tableName = sprintf('BehavData/Analysis/Mat Files/masterTable.mat');
+dirName = '/Users/jacksoncone/Dropbox/PostDoctoral Projects/!Experiments/Colliculus/';
